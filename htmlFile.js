0b20376526da8f0ccc6e670ad8913258f7253773
Ilya Krukov <ujfox13@gmail.com>
2022-05-13 18:03:21 +0300

    hotsave

--- htmlFile.js
+++ htmlFile.js
@@ -1,9 +1,21 @@
 const http = require("http");
+const fs = require('fs').promises;
 
 const host = 'localhost';
 const port = 8000;
 
 const requestListener = function (req, res) {
+    fs.readFile(__dirname + "/index.html")
+    .then(contents => {
+        res.setHeader("Content-Type", "text/html");
+        res.writeHead(200);
+        res.end(contents);
+    })
+    .catch((e) => {
+        res.writeHead(500);
+        res.end(e);
+        return;
+    });
 
 };
 
